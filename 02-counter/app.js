7fa739d671e1545c304629c455d9b6d98a13783a
Soga Okusami <sogafsd@yahoo.com>
2023-09-05 11:55:36 +0100

    add event listener and write logic for decrease button click

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -6,4 +6,12 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
-console.log(btns);
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    }
+    value.textContent = count;
+  });
+});

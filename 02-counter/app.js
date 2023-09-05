109d90760b438b7c890b76d638fc65291ee969a3
Soga Okusami <sogafsd@yahoo.com>
2023-09-05 11:58:26 +0100

    add increase and reset button functionality

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -11,6 +11,10 @@ btns.forEach(function (btn) {
     const styles = e.currentTarget.classList;
     if (styles.contains("decrease")) {
       count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
     }
     value.textContent = count;
   });

bdfe0722e4eaf30e876793a4096e38ac88a881e8
Soga Okusami <sogafsd@yahoo.com>
2023-09-15 16:01:20 +0100

    complete challenge to add and remove css properties on button click event

--- 06-modal/app.js
+++ 06-modal/app.js
@@ -2,3 +2,15 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+const openBtn = document.querySelector('.modal-btn');
+const overlayEl = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+openBtn.addEventListener('click', function() {
+  overlayEl.classList.add('open-modal');
+  
+})
+
+closeBtn.addEventListener('click', function() {
+  overlayEl.classList.remove('open-modal');
+})
